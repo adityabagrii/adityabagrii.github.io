6bbbfd5074ca09aaa3467f87f01b6e41435ec148
Aditya Bagri <aditya22029@iiitd.ac.in>
2025-07-26 07:50:27 +0530

    updated code

--- vite.config.ts
+++ vite.config.ts
@@ -21,22 +21,20 @@
 //   },
 // }));
 
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/", // ✅ Add this line
+  base: "/", 
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
